94686f7efc42d15515504b17bb5e509d7476c3b2
pandeyankit11 <74775903+pandeyankit11@users.noreply.github.com>
2025-10-20 07:41:37 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.4 MB (56.29%)

---
+++ solutions/0205-isomorphic-strings/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        
+       unordered_map<char, char> s_to_t;
+        unordered_map<char, char> t_to_s;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c1 = s[i];
+            char c2 = t[i];
+
+            // If mapping already exists, check consistency
+            if (s_to_t.count(c1)) {
+                if (s_to_t[c1] != c2) return false;
+            } else {
+                s_to_t[c1] = c2;
+            }
+
+            // Also check reverse mapping
+            if (t_to_s.count(c2)) {
+                if (t_to_s[c2] != c1) return false;
+            } else {
+                t_to_s[c2] = c1;
+            }
+        }
+        return true;
+    }
+};
