f9e52e654a4d9b088a90ae66f5cccf920c67ade3
pandeyankit11 <74775903+pandeyankit11@users.noreply.github.com>
2025-11-25 04:07:38 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.9 MB (83.84%)

--- solutions/0040-combination-sum-ii/solution.cpp
+++ solutions/0040-combination-sum-ii/solution.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-    void combination(int idx,vector<int> &arr,int target,vector<int> &temp,vector<vector<int>> &ans){
+    void combination(int i,vector<int> &arr,int target,vector<int> &temp,vector<vector<int>> &ans){
 
-        
-            if(target==0){
+        if(target==0){
             ans.push_back(temp);
             return;
             }
-        for (int j = idx; j < (int)arr.size(); j++) {
-            // skip duplicates at same level
-            if (j > idx && arr[j] == arr[j - 1]) continue;
 
-            if (arr[j] > target) break; // pruning
+        for(int j=i;j<arr.size();j++){
+            if(j>i && arr[j]==arr[j-1]) continue;
+            if(arr[j]>target) break;
 
             temp.push_back(arr[j]);
-            combination(j + 1, arr, target - arr[j], temp, ans);
+            combination(j+1,arr,target-arr[j],temp,ans);
             temp.pop_back();
-        }
+        }    
     }
 
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-    sort(candidates.begin(),candidates.end());
-     
-     vector<int>temp;
-     vector<vector<int>>ans;
-     combination(0,candidates,target,temp,ans);
-     
-     
-     return ans;   
+        sort(candidates.begin(),candidates.end());
+        
+        vector<int>temp;
+        vector<vector<int>>ans;
+        
+        combination(0,candidates,target,temp,ans);
+        
+        return ans;   
     }
 };
+
