d4aa66b2103f6dba5e518319120c7a041f8c8056
pandeyankit11 <74775903+pandeyankit11@users.noreply.github.com>
2025-12-23 07:03:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 14 MB (75.46%)

--- solutions/0039-combination-sum/solution.cpp
+++ solutions/0039-combination-sum/solution.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     void printSum(int i,vector<int> &temp, int target,vector<int> &arr,vector<vector<int>> &ans){
-     if(i==arr.size()){
+     
         if(target==0){
         ans.push_back(temp);
-        
-        }
         return;
-     }
-     if(arr[i]<=target){
-     temp.push_back(arr[i]);
-     printSum(i,temp,target-arr[i],arr,ans);
-     temp.pop_back();
-     }
+        }
+     
+     for(int j=i;j<arr.size();j++){
+            
+            if(arr[j]>target) break;
 
-     printSum(i+1,temp,target,arr,ans);
+            temp.push_back(arr[j]);
+            printSum(j,temp,target-arr[j],arr,ans);
+            temp.pop_back();
+        }    
+      
+     
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
         vector<vector<int>> ans;
         vector<int>temp;
         printSum(0,temp,target,candidates,ans);
