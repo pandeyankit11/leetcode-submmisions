f9efe3fe38a17747b4e6b3cc3b22e77c50d2f523
pandeyankit11 <74775903+pandeyankit11@users.noreply.github.com>
2025-12-25 07:07:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.5 MB (50.55%)

--- solutions/0070-climbing-stairs/solution.cpp
+++ solutions/0070-climbing-stairs/solution.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-    int solve(int i,int n,vector<int> &dp){
-        if(i==n) return 1;
-        if(i>n) return 0;
+    int solve(int i,vector<int> &dp){
+        if(i==0) return 1;
+        if(i<0) return 0;
         if(dp[i]!=-1) return dp[i];
-        return dp[i] = solve(i+1,n,dp) + solve(i+2,n,dp);
-        
+        int left=solve(i-1,dp);
+        int right =solve(i-2,dp);
+        return dp[i] = left + right;
     }
-
-    int climbStairs(int n) {
-        
-        vector<int>dp(n+1,-1);
-        return solve(0,n,dp);
-    
-        
+       int climbStairs(int n) {
+         vector<int>dp(n+1,-1);
+        return solve(n,dp);
     }
 };
