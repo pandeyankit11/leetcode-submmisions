5a522966b2ece961436370065c4ced7a8611da92
pandeyankit11 <74775903+pandeyankit11@users.noreply.github.com>
2025-12-30 17:37:35 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12 MB (31.07%)

---
+++ solutions/0063-unique-paths-ii/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int solve(int i,int j,vector<vector<int>> &arr,vector<vector<int>>&dp){
+        if(i==0 && j==0 && arr[0][0]!=1) return 1;
+        if(i<0 || j<0 || arr[i][j]==1) return 0;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        int left = solve(i-1,j,arr,dp);
+        int right = solve(i,j-1,arr,dp);
+
+        return dp[i][j]=left +right;
+
+
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        
+        int m=obstacleGrid.size();
+        int n=obstacleGrid[0].size();
+        
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return solve(m-1,n-1,obstacleGrid,dp);
+    }
+};
