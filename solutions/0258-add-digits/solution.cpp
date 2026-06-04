81fdf95b3adab621f705dc381f16841e5535e986
pandeyankit11 <74775903+pandeyankit11@users.noreply.github.com>
2026-06-04 18:11:21 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.7 MB (46.07%)

---
+++ solutions/0258-add-digits/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        int sum=0;
+        while(num>0){
+            int rem=num%10;
+            sum+=rem;
+            num/=10;
+
+            if(num==0 && sum/10 !=0){
+                num=sum;
+                sum=0;
+            }
+        }
+        return sum;
+    }
+};
