85febf9fb24fb0bcb8933a7cdff6923e422e21d2
pandeyankit11 <74775903+pandeyankit11@users.noreply.github.com>
2026-06-05 06:46:59 +0000

    Sync LeetCode submission Runtime - 3 ms (50.71%), Memory - 23.5 MB (82.94%)

--- solutions/0229-majority-element-ii/solution.cpp
+++ solutions/0229-majority-element-ii/solution.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int>ans;
-        unordered_map<int,int>m;
-
-        for(int i=0;i<nums.size();i++){
-            m[nums[i]]++;
+       int el1=0,el2=0;
+       int freq1=0,freq2=0;
+       int limit=floor(nums.size()/3);
+       vector<int>ans;
+       for(int i=0;i<nums.size();i++){
+        if(freq1==0 && nums[i]!=el2){
+             el1=nums[i];
+             freq1=1;
+        }
+        else if(freq2==0 && nums[i]!=el1){
+            el2=nums[i];
+            freq2=1;
         }
 
-        int limit=nums.size()/3;
-        for(auto it=m.begin();it!=m.end();it++){
-            if(it->second >limit)
-            ans.push_back(it->first);
+        else if(el1==nums[i]){
+            freq1++;
         }
-        return ans;
+        else if(el2==nums[i]){
+        freq2++;
+        }
+        else{
+            freq1--;
+            freq2--;
+        }
+    }
+
+    int ans1=count(nums.begin(),nums.end(),el1);
+    int ans2=count(nums.begin(),nums.end(),el2);
+
+    if(ans1>limit) ans.push_back(el1);
+    if(ans2>limit && el2!=el1) ans.push_back(el2);
+    return ans;
     }
 };
