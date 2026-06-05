73558705edebeed55b04fb528b80a6cfd023bda1
pandeyankit11 <74775903+pandeyankit11@users.noreply.github.com>
2026-06-05 20:00:33 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.8 MB (97.15%)

--- solutions/2050-count-good-numbers/solution.cpp
+++ solutions/2050-count-good-numbers/solution.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
-        long long modPow(long long base, long long exp, long long mod) {
-    long long result = 1;
-    base %= mod;
-    while (exp > 0) {
-        if (exp % 2 == 1)
-            result = (result * base) % mod;
-        base = (base * base) % mod;
-        exp /= 2;
+
+
+    long long modPow(long long base,long long exp,long long mod){
+        long long ans=1;
+        base=base%mod;
+
+        while(exp>0){
+            if(exp%2!=0)
+            ans=(ans*base)%mod;
+
+            base=(base*base)%mod;
+            exp/=2;
+        }
+
+        return ans;
     }
-    return result;
-}
-
-        int countGoodNumbers(long long n) {
-            const int MOD = 1e9 + 7;
-        long long power_four,power_five;
-        if(n%2==0){
-            power_four=n/2;
-            power_five=n/2;
-        }
-        else{
-            power_four=floor((double)n/2);
-            power_five=ceil((double)n/2);
-        }
-
-        
-
-
-long long ans = (modPow(4, power_four, MOD) * modPow(5, power_five, MOD)) % MOD;
-return ans;
-
+    int countGoodNumbers(long long n) {
+        if(n==1) return 5;
+        long long mod=1e9+7;
+        int first=modPow(5,n-n/2,mod);
+        int second=modPow(4,n/2,mod);
+        return (1LL*first*second)%mod;
     }
 };
