c1194c7f1df13a4385e130db2a585e563b41aa30
pandeyankit11 <74775903+pandeyankit11@users.noreply.github.com>
2026-06-05 15:23:32 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.6 MB (42.98%)

--- solutions/0050-powx-n/solution.cpp
+++ solutions/0050-powx-n/solution.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
+
+    double Pow(double base,long long exp){
+
+        if(exp<0){
+            base=1/base;
+            exp=-exp;
+        }
+        double ans=1;
+        while(exp>0){
+            if(exp%2!=0)
+            ans=ans*base;
+
+            base=base*base;
+            exp/=2;
+        }
+
+        return ans;
+      
+    }
     double myPow(double x, int n) {
-        return pow(x,n);
+        
+       return Pow(x,n);
     }
 };
