30ab5cd93b2e6bba06b47f63bdc251a17f5b8ec5
pandeyankit11 <74775903+pandeyankit11@users.noreply.github.com>
2026-06-10 04:52:38 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10 MB (59.99%)

--- solutions/0078-subsets/solution.cpp
+++ solutions/0078-subsets/solution.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    void solve(int i,vector<int> &temp, vector<int> &nums,vector<vector<int>> &ans){
-        
+
+    void solve(int i,vector<int>&temp,vector<vector<int>>&ans,vector<int>&nums){
+        if(i==nums.size()){
             ans.push_back(temp);
-        
-
-        for(int j=i;j<nums.size();j++){
-            temp.push_back(nums[j]);
-            solve(j+1,temp,nums,ans);
-            temp.pop_back();
+            return;
         }
+
+        temp.push_back(nums[i]);
+        solve(i+1,temp,ans,nums);
+        temp.pop_back();
+        solve(i+1,temp,ans,nums);
     }
-
-
     vector<vector<int>> subsets(vector<int>& nums) {
-        
         vector<int>temp;
         vector<vector<int>>ans;
-        solve(0,temp,nums,ans);
+        solve(0,temp,ans,nums);
         return ans;
     }
 };
