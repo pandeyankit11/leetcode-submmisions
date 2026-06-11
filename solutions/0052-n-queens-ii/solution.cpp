a033cd22ea619e04654bb79a9fb6185a75fa7b61
pandeyankit11 <74775903+pandeyankit11@users.noreply.github.com>
2026-06-11 04:32:28 +0000

    Sync LeetCode submission Runtime - 7 ms (13.72%), Memory - 15 MB (5.00%)

---
+++ solutions/0052-n-queens-ii/solution.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+     bool isSafe(int row, int col, vector<string>& board, int n) {
+
+        int duprow=row;
+        int dupcol=col;
+        while(row>=0 && col>=0){
+            if(board[row][col]=='Q')
+            return false;
+            row--;
+            col--;
+        }
+        
+        row=duprow;
+        col=dupcol;
+
+        while(col>=0){
+            if(board[row][col]=='Q')
+            return false;
+            col--;
+        }
+
+        row=duprow;
+        col=dupcol;
+
+        while(row<n && col>=0){
+            if(board[row][col]=='Q')
+            return false;
+            row++;
+            col--;
+        }
+        
+        return true;
+    }
+
+    void solve(int col, int n, vector<string>board,vector<vector<string>>& ans) {
+        if (col == n) {
+            ans.push_back(board);
+            return;
+        }
+
+        for (int row = 0; row < n; row++) {
+            if (isSafe(row, col, board, n)) {
+                board[row][col] = 'Q';
+
+                solve(col + 1, n, board, ans);
+                board[row][col] = '.';
+            }
+        }
+    }
+    int totalNQueens(int n) {
+             string s(n, '.');
+
+        vector<string> board(n);
+
+        for (int i = 0; i < n; i++) {
+            board[i] = s;
+        }
+
+        vector<vector<string>> ans;
+
+        solve(0, n, board, ans);
+        return ans.size();
+    }
+};
