ac22dc75990b5b4af902336082c908adc556ee76
pandeyankit11 <74775903+pandeyankit11@users.noreply.github.com>
2026-06-12 09:15:41 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.4 MB (88.34%)

--- solutions/0046-permutations/solution.cpp
+++ solutions/0046-permutations/solution.cpp
@@ -2,25 +2,27 @@ class Solution {
 public:
 
     void solve(vector<int>&vis, vector<int>&temp,vector<vector<int>>&ans,vector<int>&nums){
+
         if(temp.size()==nums.size()){
           ans.push_back(temp);
           return;
         }
 
-
-
         for(int j=0;j<nums.size();j++){
-            if(vis[j]) continue;
+
+            if(!vis[j]){
             vis[j]=1;
             temp.push_back(nums[j]);
             solve(vis,temp,ans,nums);
             temp.pop_back();
             vis[j]=0;
+            }
+            
         }
+     }
 
-
-    }
     vector<vector<int>> permute(vector<int>& nums) {
+
         vector<int>temp;
         vector<vector<int>>ans;
         vector<int>vis(nums.size(),0);
