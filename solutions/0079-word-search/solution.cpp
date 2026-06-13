6df74b287e529f242f054fcf1241e5e6a46a3e23
pandeyankit11 <74775903+pandeyankit11@users.noreply.github.com>
2026-06-13 16:05:54 +0000

    Sync LeetCode submission Runtime - 307 ms (46.09%), Memory - 10.5 MB (82.04%)

--- solutions/0079-word-search/solution.cpp
+++ solutions/0079-word-search/solution.cpp
@@ -1,33 +1,47 @@
 class Solution {
 public:
+    vector<vector<int>>dir={
+        {1,0},
+        {0,-1},
+        {0,1},
+        {-1,0}
+    };
 
-    bool solve(int i,int row,int col,vector<vector<char>>&board,string &word){
-     if(i==word.size())
-     return true;
-     
-     if(row<0 || col >=board[0].size() || col<0 || row>=board.size())
-     return false;
+    bool solve(int i, int row, int col, vector<vector<char>>& board,
+               string& word) {
 
-     if(board[row][col]=='$') return false;
-     if(board[row][col]!=word[i]) return false;
-     char temp = board[row][col];
-     board[row][col]='$';
+        if (i == word.size())
+            return true;
 
-     if(solve(i+1,row+1,col,board,word)||solve(i+1,row-1,col,board,word)||solve(i+1,row,col+1,board,word)||solve(i+1,row,col-1,board,word)){
+        if (row < 0 || col >= board[0].size() || col < 0 || row >= board.size()||board[row][col] == '$'||board[row][col] != word[i])
+            return false;
+        
+        char temp=board[row][col];
+        board[row][col]='$';
+
+        for(int k=0;k<4;k++){
+            if(solve(i+1,row+dir[k][0],col+dir[k][1],board,word)){
+                board[row][col]=temp;
+                return true;
+            }
+        }
+
         board[row][col]=temp;
-        return true;
-     }
-     board[row][col]=temp;
-     return false;
+
+        return false;
+       
+
+       
+
     }
     bool exist(vector<vector<char>>& board, string word) {
-        int row=board.size();
-        int col=board[0].size();
+        int row = board.size();
+        int col = board[0].size();
 
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(solve(0,i,j,board,word))
-                return true;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (solve(0, i, j, board, word))
+                    return true;
             }
         }
         return false;
