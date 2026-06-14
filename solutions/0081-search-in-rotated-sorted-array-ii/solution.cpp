db557b3478d2b3b7043ef3dbd2bd6607978d3de4
pandeyankit11 <74775903+pandeyankit11@users.noreply.github.com>
2026-06-14 06:26:46 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18 MB (42.24%)

--- solutions/0081-search-in-rotated-sorted-array-ii/solution.cpp
+++ solutions/0081-search-in-rotated-sorted-array-ii/solution.cpp
@@ -1,10 +1,34 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        auto it=find(nums.begin(),nums.end(),target);
-        if(it!=nums.end())
-        return true;
+        int l=0;
+       int r=nums.size()-1;
 
-        return false;
+       while(l<=r){
+        int mid=(l+r)/2;
+
+        if(nums[mid]==target)
+        return true;
+        
+        if(nums[l]==nums[mid] && nums[mid]==nums[r]){
+            l++;
+            r--;
+            continue;
+        }
+
+        if(nums[l]<=nums[mid]){
+            if(target>=nums[l] && target<nums[mid])
+            r=mid-1;
+            else
+            l=mid+1;
+        }
+        else{
+            if(target>nums[mid] && target<=nums[r])
+            l=mid+1;
+            else
+            r=mid-1;
+        }
+       }
+       return false;
     }
 };
