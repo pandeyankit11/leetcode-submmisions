7dc3909231b84d78dc087525fc434ae8f794adaf
pandeyankit11 <74775903+pandeyankit11@users.noreply.github.com>
2026-06-14 04:19:07 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 31.4 MB (7.26%)

--- solutions/0792-binary-search/solution.cpp
+++ solutions/0792-binary-search/solution.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        auto it =find(nums.begin(),nums.end(),target);
-        if(it!=nums.end())
-        return it-nums.begin();
-        else{
-            return -1;
+        int l=0;
+        int r=nums.size()-1;
+
+        while(l<=r){
+            int mid=(l+r)/2;
+
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]>target)
+            r=mid-1;
+            else
+            l=mid+1;
         }
+
+        return -1;
     }
 };
