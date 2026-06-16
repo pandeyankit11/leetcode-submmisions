8d8943905331d8ccefb51e17400931255e6d14a3
pandeyankit11 <74775903+pandeyankit11@users.noreply.github.com>
2026-06-16 13:08:14 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10 MB (38.43%)

--- solutions/0125-valid-palindrome/solution.cpp
+++ solutions/0125-valid-palindrome/solution.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
-    bool check_pd(string &s,int i,int n){
-    if(i>=n/2)
-    return true;
-    if(s[i]!=s[n-i-1])
-    return false;
-    return check_pd(s,i+1,n);
-    }
     bool isPalindrome(string s) {
-        string clean="";
-        for(auto c:s){
-            if(isalnum(c)){
-                if(isupper(c))
-                c=tolower(c);
-                clean+=c;
+        int left=0;
+        int right=s.length()-1;
+
+        while(left<right){
+
+            if(!isalnum(s[left])){
+            left++;
+            continue;
             }
-         
-        }
-        return check_pd(clean,0,clean.size());
+
+            if(!isalnum(s[right])){
+            right--;
+            continue;
+            }
+
+            if(tolower(s[left])!=tolower(s[right]))
+            return false;
+
+            left++;
+            right--;
+
+            }
+
+            return true;
     }
 };
