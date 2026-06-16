b2bf3162a710b7bce6d1acab058ef9949dbdc387
pandeyankit11 <74775903+pandeyankit11@users.noreply.github.com>
2026-06-16 20:18:37 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 23.9 MB (55.61%)

--- solutions/0283-move-zeroes/solution.cpp
+++ solutions/0283-move-zeroes/solution.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1) return ;
+        int n = nums.size();
+        
 
-        for(int i=0,j=1;j<n;j++){
-            if(nums[i]==0 && nums[j]!=0){
-                int temp=nums[i];
-                nums[i]=nums[j];
-                nums[j]=temp;
-                i++;
+        int slow = 0;
+
+        for (int fast = 0; fast < n; fast++) {
+            if (nums[fast] != 0) {
+                swap(nums[slow], nums[fast]);
+                slow++;
             }
-            if((nums[i]!=0 && nums[j]==0) || (nums[i]!=0 && nums[j]!=0))
-            i++;
         }
     }
 };
