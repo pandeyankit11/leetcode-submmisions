da76a81a2e4816d78f22d90a3dbb7ceeecc176d6
pandeyankit11 <74775903+pandeyankit11@users.noreply.github.com>
2026-06-18 18:53:15 +0000

    Sync LeetCode submission Runtime - 3 ms (25.82%), Memory - 12.6 MB (82.76%)

---
+++ solutions/1047-maximize-sum-of-array-after-k-negations/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int largestSumAfterKNegations(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<=0 && k>0){
+                nums[i]*=-1;
+                k--;
+            }
+        }
+
+        int sum=accumulate(nums.begin(),nums.end(),0);
+
+        int min=*min_element(nums.begin(),nums.end());
+
+        if(k%2==0) return sum;
+
+        return sum-(2*min);
+    }
+};
