d5108ca14bc3d788b0c8a565ed66997423739752
pandeyankit11 <74775903+pandeyankit11@users.noreply.github.com>
2026-06-18 03:28:17 +0000

    Sync LeetCode submission Runtime - 35 ms (95.61%), Memory - 94.1 MB (39.22%)

--- solutions/0435-non-overlapping-intervals/solution.cpp
+++ solutions/0435-non-overlapping-intervals/solution.cpp
@@ -1,37 +1,24 @@
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        int n=intervals.size();
-        if (n==1) return 0;
-        vector<pair<int,int>>vec;
-        int ans{0};
-        for(int i=0;i<n;i++){
-           vec.push_back({intervals[i][0],intervals[i][1]});
+
+        sort(intervals.begin(), intervals.end(),
+             [](const vector<int>& a, const vector<int>& b) {
+                 return a[1] < b[1];
+             });
+
+        int remove_count = 0;
+
+        int lastValidEnd = intervals[0][1];
+
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[i][0] < lastValidEnd) {
+                remove_count++;
+            } else {
+                lastValidEnd = intervals[i][1];
+            }
         }
 
-      stable_sort(vec.begin(), vec.end(),
-    [](const pair<int,int> &a,const pair<int,int> &b){
-
-        return a.first < b.first;
-    });
-        int last_second=vec[0].second;
-        for(int i=1;i<vec.size();i++){
-            int curr_first=vec[i].first;
-            int curr_second=vec[i].second;
-
-            if(curr_first<last_second && curr_second<last_second){
-                ans++;
-                last_second=curr_second;
-                
-            }
-            else if(curr_first<last_second){
-                ans++;
-            }
-            else{
-                last_second=curr_second;
-            }
-
-        }
-        return ans;
+        return remove_count;
     }
 };
