5d8936495031e95b335374ac713a0012d5d0d46e
pandeyankit11 <74775903+pandeyankit11@users.noreply.github.com>
2026-06-19 16:51:55 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 21.9 MB (13.71%)

--- solutions/0045-jump-game-ii/solution.cpp
+++ solutions/0045-jump-game-ii/solution.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
-     
     int jump(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1) return 0;
+       int n=nums.size();
+        if (n == 1)
+            return 0;
+        vector<int>vec(n,0);
 
-       int l{0},r{0};
-       int jumps{0};
+        for(int i=0;i<n;i++){
+            vec[i]=i+nums[i];
+        }
 
-       while(r<n-1){
+        int jumps = 0;
+        int l = 0;
+        int r = 0;
         int farthest=0;
 
-        for(int i=l;i<=r;i++){
-            farthest=max(farthest,i+nums[i]);
+        while(r<n-1){
+
+            for(int i=l;i<=r;i++){
+                farthest=max(farthest,vec[i]);
+            }
+
+            l=r+1;
+            r=farthest;
+            jumps++;
         }
-        
-        l=r+1;
-        r=farthest;
-        jumps++;
-       }
-     
-     return jumps;
-     }
+
+        return jumps;
+    }
 };
