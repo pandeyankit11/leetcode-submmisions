f447b6c8110e3fc3485955c78c6a01a6b524e294
pandeyankit11 <74775903+pandeyankit11@users.noreply.github.com>
2026-06-21 13:29:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.5 MB (46.04%)

--- solutions/0402-remove-k-digits/solution.cpp
+++ solutions/0402-remove-k-digits/solution.cpp
@@ -3,7 +3,7 @@ public:
     string removeKdigits(string num, int k) {
         stack<char>st;
         for(int i=0;i<num.size();i++){
-        while(!st.empty() && k>0 && (st.top()-'0')>(num[i]-'0')){
+        while(!st.empty() && k>0 && st.top()>num[i]){
             st.pop();
             k--;
         }
@@ -19,13 +19,16 @@ public:
         if(st.empty()) return "0";
 
         string res="";
+
         while(!st.empty()){
             res+=st.top();
             st.pop();
         }
+
         while(!res.empty() && res.back()=='0'){
             res.pop_back();
         }
+        
         if(res.empty()) return "0";
 
         reverse(res.begin(),res.end());
