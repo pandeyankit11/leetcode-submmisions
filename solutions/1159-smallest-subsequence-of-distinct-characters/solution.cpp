e462f3d5a94f7a9a73131cc3c94f8ce8e8579265
pandeyankit11 <74775903+pandeyankit11@users.noreply.github.com>
2026-06-21 15:00:04 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.8 MB (35.83%)

---
+++ solutions/1159-smallest-subsequence-of-distinct-characters/solution.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        
+        vector<int>lastOccurence(26,0);
+      
+        for(int i=0;i<s.size();i++){
+            lastOccurence[s[i]-'a']=i;
+        }
+
+        stack<char>st;
+        vector<bool> seen(26, false);
+
+        for(int i=0;i<s.size();i++){
+
+            if(seen[s[i]-'a']) continue;
+
+            while(!st.empty() && st.top()>s[i] && lastOccurence[st.top()-'a']>i){
+                seen[st.top()-'a']=false;
+                st.pop();
+            }
+
+            st.push(s[i]);
+            seen[s[i]-'a']=true;
+        }
+
+        string res="";
+        while(!st.empty()){
+            res+=st.top();
+            st.pop();
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
