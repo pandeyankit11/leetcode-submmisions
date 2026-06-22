750c1bfcf36184b18acb88668e10761fd6fe37a4
pandeyankit11 <74775903+pandeyankit11@users.noreply.github.com>
2026-06-22 06:45:48 +0000

    Sync LeetCode submission Runtime - 7 ms (15.23%), Memory - 18 MB (39.06%)

--- solutions/0347-top-k-frequent-elements/solution.cpp
+++ solutions/0347-top-k-frequent-elements/solution.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        
-        unordered_map<int,int>m;
-        vector<int>ans(k);
 
-        for(int i:nums){
+        unordered_map<int, int> m;
+        vector<int> ans;
+
+        for (int i : nums) {
             m[i]++;
         }
 
-    vector<pair<int,int>> vec(m.begin(), m.end());
+        priority_queue<pair<int, int>, vector<pair<int, int>>,greater<pair<int, int>>>pq;
 
-     sort(vec.begin(), vec.end(),[](pair<int,int> &a, pair<int,int> &b){
-        return a.second > b.second;
-    });
-    for(int i=0;i<k;i++){
-        ans[i]=vec[i].first;
-    }
-    return ans;
+        for (auto it = m.begin(); it != m.end(); it++) {
+            pq.push({it->second, it->first});
 
+            if (pq.size() > k) {
+                pq.pop();
+            }
+        }
 
+        while (!pq.empty()) {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+
+        return ans;
     }
 };
