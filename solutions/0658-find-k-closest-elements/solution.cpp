cd8d52abb742df7bd1022050fed98750a002bdee
pandeyankit11 <74775903+pandeyankit11@users.noreply.github.com>
2026-06-22 07:59:09 +0000

    Sync LeetCode submission Runtime - 26 ms (24.68%), Memory - 38.7 MB (28.17%)

---
+++ solutions/0658-find-k-closest-elements/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        vector<int>ans;
+
+        priority_queue<pair<int,int>>pq;
+
+        for(int i=0;i<arr.size();i++){
+            int dist=abs(arr[i]-x);
+
+            pq.push({dist,arr[i]});
+
+            if(pq.size()>k)
+            pq.pop();
+        }
+
+        while(!pq.empty()){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
