d4756f94832e20d8d075bba58fdbcbfb85177469
pandeyankit11 <74775903+pandeyankit11@users.noreply.github.com>
2026-06-23 11:57:12 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 21.9 MB (38.18%)

--- solutions/0268-missing-number/solution.cpp
+++ solutions/0268-missing-number/solution.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        bool check{0};
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]-i!=0){
-            check=1;
-            return i;
-            }
+        int ans=0;
+
+        for(int i=0;i<=nums.size();i++){
+            ans^=i;
         }
-        
-        return nums.size();
+
+        for(int i=0;i<nums.size();i++){
+         ans^=nums[i];
+        }
+
+        return ans;
     }
 };
