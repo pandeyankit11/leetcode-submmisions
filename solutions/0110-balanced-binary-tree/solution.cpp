c81b99e0e817c724edd2272c7a8e89e7f34959b4
pandeyankit11 <74775903+pandeyankit11@users.noreply.github.com>
2026-06-25 06:21:53 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 22.9 MB (83.26%)

--- solutions/0110-balanced-binary-tree/solution.cpp
+++ solutions/0110-balanced-binary-tree/solution.cpp
@@ -11,20 +11,22 @@
  */
 class Solution {
 public:
+        int height(TreeNode* root,bool &balanced){
+        if(root==nullptr) return 0;
+
+        int lh=height(root->left,balanced);
+        int rh=height(root->right,balanced);
 
     
+        if(abs(lh-rh)>1)
+        balanced=false;
 
-    int height(TreeNode* root){
-        if(root==NULL ) return 0;
-
-        int lh=height(root->left);
-        int rh=height(root->right);
-        if(lh==-1 || rh==-1) return -1;
-        if(abs(lh-rh)>1) return -1;
         return 1+max(lh,rh);
     }
 
     bool isBalanced(TreeNode* root) {
-        return height(root)!=-1;
+        bool balanced=true;
+        height(root,balanced);
+        return balanced;
     }
 };
