5be870f6419cd114aba46befdf490a8f000c86f3
pandeyankit11 <74775903+pandeyankit11@users.noreply.github.com>
2026-06-26 12:29:48 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 35.4 MB (63.89%)

--- solutions/0783-search-in-a-binary-search-tree/solution.cpp
+++ solutions/0783-search-in-a-binary-search-tree/solution.cpp
@@ -6,18 +6,19 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-
-       
     TreeNode* searchBST(TreeNode* root, int val) {
-       while(root!=NULL && root->val!=val){
-        root= (val < root->val) ? (root->left):(root->right); 
-       }
-       return root;
+         if(!root) return nullptr;
         
+        if(root->val==val) return root;
+        
+        if(val<root->val) return searchBST(root->left,val);
+        
+        return searchBST(root->right,val);
     }
 };
